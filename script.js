0c341e43efbcdb128eb300af38b30fb2d5529868
Geghard-S <geghard@gmail.com>
2021-05-17 23:54:34 -0700

    second commit

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const  API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=140a4dc2f0ad78e864f1e623e2304d3e&page=1'
 const IMG_PATH = 'https=//image.tmdb.org/t/p/w1280'
-const SEARCH_URL = 'https://api.themoviedb.org/3/search/movie?&api_key=140a4dc2f0ad78e864f1e623e2304d3e&query="'
+const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=140a4dc2f0ad78e864f1e623e2304d3e&query="'
+
+const form = document.getElementById('form')
+const search = document.getElementById('search')
 
 getMovies(API_URL)
 
@@ -10,4 +13,18 @@ async function getMovies(url) {
 
     console.log(data.results)
 }
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
+
+    const searchTerm = search.value
+
+    if(searchTerm && searchTerm !== '') {
+        getMovies(SEARCH_API + searchTerm)
+
+        search.value = ''
+    } else {
+        window.location.reload()
+    }
+})
 
