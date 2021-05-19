127cce7602a3cc8de8d8e79ed2f3771551faaa50
Geghard-S <geghard@gmail.com>
2021-05-18 22:22:50 -0700

    third commit

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 const  API_URL = 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=140a4dc2f0ad78e864f1e623e2304d3e&page=1'
-const IMG_PATH = 'https=//image.tmdb.org/t/p/w1280'
+const IMG_PATH = 'https://image.tmdb.org/t/p/w1280'
 const SEARCH_API = 'https://api.themoviedb.org/3/search/movie?&api_key=140a4dc2f0ad78e864f1e623e2304d3e&query="'
 
+const main = document.getElementById('main')
 const form = document.getElementById('form')
 const search = document.getElementById('search')
 
@@ -11,7 +12,43 @@ async function getMovies(url) {
     const res = await fetch(url)
     const data = await res.json()
 
-    console.log(data.results)
+    showMovies(data.results)
+}
+
+function showMovies (movies) {
+    main.innerHTML = ''
+
+    movies.forEach((movie) => {
+        const { title, poster_path, vote_average, overview } = movie
+
+        const movieEl = document.createElement('div')
+        movieEl.classList.add('movie')
+
+        movieEl.innerHTML = `
+        <div class="movie">
+            <img src="${IMG_PATH + poster_path}" alt="${title}">
+            <div class="movie-info">
+                <h3>${title}</h3>
+                <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+            </div>
+            <div class="overview">
+                <h3>Overview</h3>
+                ${overview}
+            </div>
+        </div>
+        `
+        main.appendChild(movieEl)
+    })
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green'
+    }else if(vote >= 5){
+        return 'orange'
+    }else{
+        return 'red'
+    }
 }
 
 form.addEventListener('submit', (e) => {
@@ -27,4 +64,6 @@ form.addEventListener('submit', (e) => {
         window.location.reload()
     }
 })
+
+
 
